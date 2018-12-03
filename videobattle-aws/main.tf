26d5bbf3cdc0831967b86838f622b09ebbe3c61e
Dartblack <belek.eraliev@gmail.com>
2018-12-03 19:34:58 +0600

    added random_string for name and terraform fmt

--- videobattle-aws/main.tf
+++ videobattle-aws/main.tf
@@ -2,3 +2,12 @@ provider "aws" {
   region = "${var.region}"
 }
 
+resource "random_string" "name" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+locals {
+  name = "${var.name}-${terraform.env}-${random_string.name.result}-${var.short_region}"
+}
