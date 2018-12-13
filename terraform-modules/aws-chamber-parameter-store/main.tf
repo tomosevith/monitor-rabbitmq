f2dee9a8b1925ce4c4743f6c3486203ed02ae873
Aziz <aziz.sadyk@gmail.com>
2018-12-13 19:54:40 +0600

    change kms_key from securestring to string and comment key_id

--- terraform-modules/aws-chamber-parameter-store/main.tf
+++ terraform-modules/aws-chamber-parameter-store/main.tf
@@ -2,8 +2,9 @@ resource "aws_ssm_parameter" "main" {
   count     = "${var.count}"
   name      = "/${var.name}/${element(keys(var.parameters), count.index)}"
   value     = "${element(values(var.parameters), count.index)}"
-  type      = "SecureString"
-  key_id    = "${var.kms_key_id}"
+  /*type      = "SecureString"*/
+  type      = "String"
+  /*key_id    = "${var.kms_key_id}"*/
   overwrite = true
 
   tags = "${var.tags}"
