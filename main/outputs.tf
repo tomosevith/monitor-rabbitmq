ececbd56ed0fb60f2ab90350983bbf46209622a8
halfb00t <halfb00t@gmail.com>
2018-12-14 17:32:17 +0600

    Fixed rmq module output, added outputs to the main

--- main/outputs.tf
+++ main/outputs.tf
@@ -1,3 +1,19 @@
-output "private_subnets" {
-  value = "${module.vpc.private_subnets}"
+output "console_url" {
+  value = "https://${aws_route53_record.web_console.fqdn}"
+}
+
+output "content_url" {
+  value = "https://${aws_route53_record.content.fqdn}"
+}
+
+output "backend_url" {
+  value = "https://${aws_route53_record.web_backend.fqdn}"
+}
+
+output "frontend_url" {
+  value = "https://${aws_route53_record.web_front.fqdn}"
+}
+
+output "rabbitmq_url" {
+  value = "https://${aws_route53_record.rabbitmq.fqdn}"
 }
--- terraform-modules/aws-rmq-cluster/outputs.tf
+++ terraform-modules/aws-rmq-cluster/outputs.tf
@@ -1,5 +1,5 @@
 output "rabbitmq_dns_name" {
-  value = "https://${aws_elb.elb.dns_name}"
+  value = "${aws_elb.elb.dns_name}"
 }
 
 output "rabbitmq_zone_id" {
