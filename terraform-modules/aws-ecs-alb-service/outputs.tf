835751c3fbcf9363588e42d5ffb491a18fc8a535
halfb00t <halfb00t@gmail.com>
2018-12-17 15:54:58 +0600

    Added td arn to outputs

--- terraform-modules/aws-ecs-alb-service/outputs.tf
+++ terraform-modules/aws-ecs-alb-service/outputs.tf
@@ -6,6 +6,10 @@ output "task_definition_family" {
   value = "${aws_ecs_task_definition.td.family}"
 }
 
+output "task_definition_arn" {
+  value = "${aws_ecs_task_definition.td.arn}"
+}
+
 output "service_name" {
   value = "${aws_ecs_service.ecs_service.name}"
 }
